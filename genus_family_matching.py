eed69233ed66ae5e0670a9471a17c63279ea6aa9
khu0830 <khu0830@gmail.com>
2023-11-26 20:08:49 +0900

    2023.11.26 updated

---
+++ genus_family_matching.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+gg_13_5_taxonomy = '/home/khu0830/Analysis/gg_13_5_taxonomy.tsv'
+target_genus = '/home/khu0830/Analysis/target_genus.tsv'
+
+df_gg = pd.read_csv(gg )
